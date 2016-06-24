e43609def71bf93dbedfd6dc0da4681531f61611
AkshaykumarA <akshaykumar1211@gmail.com>
2016-06-24 23:18:16 +0530

    Update swapnumber.c

--- swapnumber.c
+++ swapnumber.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main() {
    int a, b, temp;
-   printf("Enter the value of a and b: ");
+   printf("Enter the value of a and b:\n");
    scanf("%d %d", &a, &b);
    printf("Before swapping a=%d, b=%d ", a, b);
    temp = a;
